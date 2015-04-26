d1d86a4e4b2dfa627fad09b66803e78d8813b892
liuheng <liuheng@qiyi.com>
2015-04-26 21:15:30 +0800

    application config

---
+++ application/config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#!-*- coding:utf-8 -*-
+__author__ = 'bertramlau(moodspace@gmail.com)'
+"""
+application config
+"""
+DEBUG = True
+CHARSET = 'UTF-8'
+LOGIN_KEY = 'user_id'  # website login key
+ADMIN_LOGIN_KEY = 'admin_id'  # admin login key
